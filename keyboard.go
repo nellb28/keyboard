8342c49c5ce80bb0b1356303da37b7297a7a88ce
Nell Brisard <nell.brisard@coxautoinc.com>
2021-03-31 18:02:28 -0500

    add second method

--- keyboard.go
+++ keyboard.go
@@ -24,3 +24,18 @@ func GetFloat() (float64, error) {
 	}
 	return number, nil
 }
+
+
+func GetString() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	if err != nil {
+		return "0", err
+	}
+	return input, nil
+}
