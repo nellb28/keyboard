0d77c2fe3a2b2bcfc05e34a19232dd1af7bfe0f0
Nell B <nellb28@users.noreply.github.com>
2021-03-31 18:10:35 -0500

    add new getfloat func

--- keyboard.go
+++ keyboard.go
@@ -25,6 +25,21 @@ func GetFloat() (float64, error) {
 	return number, nil
 }
 
+func GetFloatTwo() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number * 2, nil
+}
+
 
 func GetString() (string, error) {
 	reader := bufio.NewReader(os.Stdin)
